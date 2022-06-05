d04b426932fe456c115c84f43cfd28d68d502100
willis chou <willis.c@binance.com>
2022-06-05 23:53:39 +0800

    get image url

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,11 @@ api_url="https://pokeapi.co/api/v2/pokemon/${INPUT_POKEMON_ID}"
 echo $api_url
 
 pokemon_name=$(curl "${api_url}" | jq ".name")
+pokemon_front=$(curl "${api_url}" | jq ".sprites.front_default")
 echo $pokemon_name
+echo $pokemon_front
 
 # In order to create an output for our Action, we need to use a GitHub Action-specific syntax
 # echo "::set-output name=<output name>::<value>"
 echo "::set-output name=pokemon_name::$pokemon_name"
+echo "::set-output name=pokemon_front::$pokemon_front"
